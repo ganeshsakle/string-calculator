1e8a6594fffaeefff8175b5ba753a4a22a5eb23c
ganeshsakle <ganeshsakle7@gmail.com>
2025-03-13 13:57:11 +0530

    Logic to allow multiple delimiters and code refactor

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class Calculator
+  DEFAULT_DELIMITER = ','
+  NEW_LINE = '\n'
+  SEMI_COLON = ';'
+
   def add(numbers)
     return 0 if numbers.empty?
 
-    delimiter, numbers = extract_delimiter(numbers)
-    numbers_array = numbers.tr('\n', delimiter).split(delimiter).map(&:to_i)
+    delimiters, numbers = extract_delimiter(numbers)
+
+    delimiters.each do |delimiter|
+      numbers = numbers.tr(delimiter, DEFAULT_DELIMITER)
+    end
+
+    numbers_array = numbers.split(DEFAULT_DELIMITER).map(&:to_i)
 
     raise_negatives_exception(numbers_array)
     ignore_big_numbers(numbers_array)
@@ -16,19 +25,29 @@ class Calculator
   private
 
   def extract_delimiter(numbers)
-    delimiter = ','
+    delimiters = [DEFAULT_DELIMITER, NEW_LINE, SEMI_COLON]
 
     if numbers.start_with?('//')
       delimiter_line, numbers = numbers.split('\n', 2)
-      delimiter = delimiter_line[-1]
-      closing_delimiter_index = delimiter_line.index(']')
+      current_delimiter = ''
 
-      if closing_delimiter_index
-        delimiter = delimiter_line[3...closing_delimiter_index]
+      delimiter_line[3..].each_char do |char|
+        if char == '['
+          current_delimiter = ''
+        elsif char == ']'
+          delimiters << current_delimiter
+          current_delimiter = ''
+        else
+          current_delimiter += char
+        end
       end
+
+      delimiters << current_delimiter unless current_delimiter.empty?
+
+      delimiters.reject!(&:empty?)
     end
 
-    [delimiter, numbers]
+    [delimiters, numbers]
   end
 
   def raise_negatives_exception(numbers_array)
