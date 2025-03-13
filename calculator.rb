a7325db61910e32ea82d9c63b733ad2a2bf1b84d
ganeshsakle <ganeshsakle7@gmail.com>
2025-03-13 11:33:51 +0530

    Added Support of different delimiters in add method

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,15 @@ class Calculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.tr('\n', ',').split(',').map(&:to_i).sum
+    delimiter = extract_delimiter(numbers)
+    numbers = numbers.sub('//[^\n]*\n', '') if numbers.start_with?('//')
+
+    numbers.tr('\n', delimiter).split(delimiter).map(&:to_i).sum
+  end
+
+  private
+
+  def extract_delimiter(numbers)
+    numbers.start_with?('//') ? numbers.split('\n', 2).first[-1] : ','
   end
 end
